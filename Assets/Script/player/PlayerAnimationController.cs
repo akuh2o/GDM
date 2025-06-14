ababba4f8275befab71bde101369e242cab37f11
luchomellone <113950224+luchomellone@users.noreply.github.com>
2025-06-14 15:25:09 -0300

    Animacion pj v2

---
+++ Assets/Script/player/PlayerAnimationController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField] private float smoothTime = 0.1f;
+    private float currentHorizontal;
+    private float currentVertical;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+       
+        currentHorizontal = Mathf.Lerp(currentHorizontal, horizontal, smoothTime);
+        currentVertical = Mathf.Lerp(currentVertical, vertical, smoothTime);
+
+       
+        animator.SetFloat("Horizontal", currentHorizontal);
+        animator.SetFloat("Vertical", currentVertical);
+    }
+}
