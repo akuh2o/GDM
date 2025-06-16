a5ce856333a7868046aaba3e5ae52ddcbc784713
steflant <estefanolanternier14@gmail.com>
2025-06-15 22:59:20 -0300

    CAMARA + FOV APUNTADO

--- Assets/Script/ARMA/shoot.cs
+++ Assets/Script/ARMA/shoot.cs
@@ -6,19 +6,46 @@ public class shoot : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
     [SerializeField] private GameObject spawnPoint;
-    // Start is called before the first frame update
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private float aimingFOV = 30f;
+    private float defaultFOV;
+    private static bool isAiming = false;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+        if (mainCamera != null)
+        {
+            defaultFOV = mainCamera.fieldOfView;
+        }
+    }void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-    {
-        Instantiate(bullet, spawnPoint.transform.position, spawnPoint.transform.rotation);
-    }
+        if (Input.GetMouseButtonDown(1))
+        {
+            isAiming = true;
+            Debug.Log("Apuntando con la mira");
+            if (mainCamera != null)
+            {
+                mainCamera.fieldOfView = aimingFOV;
+            }
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            isAiming = false;
+            Debug.Log("Dejó de apuntar");
+            if (mainCamera != null)
+            {
+                mainCamera.fieldOfView = defaultFOV;
+            }
+        }
 
+        if (isAiming && Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, spawnPoint.transform.position, spawnPoint.transform.rotation);
+        }
+   
     }
 }
